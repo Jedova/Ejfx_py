2977fe6f27d48f7fa16430f3001d42656b2c8e15
Jedova <nemergy.consulting@gmail.com>
2025-06-14 22:59:32 +0000

    avance de ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,70 @@
+#ejercicio 1
+# def saludo():
+#    print("hola")
+#saludo()
+
+
+#ejercicio 2
+
+#def imp_nom (nombre):
+#    print(f"cual es tu nombre: {nombre}")
+#imp_nom("sofia")
+
+
+#ejercicio3
+
+#def area_cir(radio):
+#    pi=3.1416
+#    return pi*radio**2
+
+#radio=float(input("incluye el radio: "))
+
+#print(area_cir(radio))
+
+
+#ejercicio 4
+
+#def operaciones_basicas(a, b):
+#    sumar = a + b
+#    restar = a - b
+#    multiplicar = a*b
+#    return sumar, restar, multiplicar
+
+#a=float(input("valor de a: "))
+#b=float(input("valor de b: "))
+
+#adicion,sustracción,multiplicar = operaciones_basicas(a, b)
+
+#print(f"Suma: {adicion}, Resta: {sustracción}, multiplicar: {multiplicar}")
+
+
+#ejercicio 5
+
+#def promedio(lista_numeros):
+#    return sum(lista_numeros) / len(lista_numeros)
+
+#a=int(input("datos a: "))
+#b=int(input("datos b: "))
+#c=int(input("datos c: "))
+#d=int(input("datos d: "))
+
+#print(promedio([a, b, c, d]))
+
+#Ejercicio 6
+
+#def saludar(nombre):
+#    print(f"Hola, {nombre}")
+
+#nombre=input("¿cual es tu nombre?: ")
+
+#saludar(nombre)
+
+#ejercicio 7
+
+#def mostrar_datos(**persona):
+#    for clave, valor in persona.items():
+#        print(f"{clave}: {valor}")
+
+#mostrar_datos(nombre="Ana", edad=25, ciudad="Valdivia")
+
+
